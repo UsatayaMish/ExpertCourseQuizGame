67f681329fcc0d47b3824393906334105d3982e9
usatayamish <147038989+LuftWaiffu@users.noreply.github.com>
2024-09-16 13:40:18 +0300

    QG-04 MainActivity

--- app/src/main/java/com/usatayamish/expertcoursequizgame/MainActivity.kt
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/MainActivity.kt
@@ -5,16 +5,56 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.usatayamish.expertcoursequizgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = GameViewModel()
+
+        binding.firstChoiceButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.chooseFirst()
+            uiState.update(binding = binding)
+        }
+
+        binding.secondChoiceButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.chooseSecond()
+            uiState.update(binding = binding)
+        }
+
+        binding.thirdChoiceButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.chooseThird()
+            uiState.update(binding = binding)
+        }
+
+        binding.fourthChoiceButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.chooseFourth()
+            uiState.update(binding = binding)
+        }
+
+        binding.checkButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.check()
+            uiState.update(binding = binding)
+        }
+
+        binding.nextButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.next()
+            uiState.update(binding = binding)
+        }
+
+        val uiState: GameUiState = viewModel.init()
+        uiState.update(binding = binding)
     }
+
+
+
 }
