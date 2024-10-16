5d2ac6e8f84c77a030698161a00baa3505499f9a
usatayamish <147038989+LuftWaiffu@users.noreply.github.com>
2024-10-16 18:45:11 +0300

    QG-07 handle activity death

---
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/IntCache.kt
@@ -0,0 +1,26 @@
+package com.usatayamish.expertcoursequizgame
+
+import android.content.SharedPreferences
+
+interface IntCache {
+
+    fun save(newValue: Int)
+
+    fun read() : Int
+
+    class Base(
+        private val sharedPreferences: SharedPreferences,
+        private val key: String,
+        private val defaultValue: Int
+    ) : IntCache {
+
+        override fun save(newValue: Int) {
+            sharedPreferences.edit().putInt(key, newValue).apply()
+        }
+
+        override fun read(): Int {
+            return sharedPreferences.getInt(key, defaultValue)
+        }
+
+    }
+}
--- app/src/main/java/com/usatayamish/expertcoursequizgame/QuizApp.kt
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/QuizApp.kt
@@ -1,14 +1,24 @@
 package com.usatayamish.expertcoursequizgame
 
 import android.app.Application
+import android.content.Context
+
 
 class QuizApp : Application() {
+
 
     lateinit var viewmodel: GameViewModel
 
     override fun onCreate() {
         super.onCreate()
-        viewmodel = GameViewModel(GameRepository.Base())
+        val sharedPreferences = getSharedPreferences(
+            "quizAppData",
+            Context.MODE_PRIVATE
+        )
+        viewmodel = GameViewModel(GameRepository.Base(
+            IntCache.Base(sharedPreferences, "indexKey", 0),
+            IntCache.Base(sharedPreferences, "userChoiceIndexKey", -1)
+        ))
 
     }
 }
