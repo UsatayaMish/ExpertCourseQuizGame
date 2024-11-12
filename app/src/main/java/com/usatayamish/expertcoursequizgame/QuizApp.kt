7096e1a3d2e13631fe31c6be0701be31c0dabe4f
usatayamish <147038989+LuftWaiffu@users.noreply.github.com>
2024-11-12 09:29:38 +0300

    QG-17 manual di

--- app/src/main/java/com/usatayamish/expertcoursequizgame/QuizApp.kt
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/QuizApp.kt
@@ -1,39 +1,33 @@
 package com.usatayamish.expertcoursequizgame
 
 import android.app.Application
-import android.content.Context
-import com.usatayamish.expertcoursequizgame.game.GameRepository
-import com.usatayamish.expertcoursequizgame.game.GameViewModel
-import com.usatayamish.expertcoursequizgame.stats.GameOverViewModel
-import com.usatayamish.expertcoursequizgame.stats.StatsRepository
 
 
-class QuizApp : Application() {
+class QuizApp : Application(), ProvideViewModel {
 
-
-    lateinit var gameViewModel: GameViewModel
-    lateinit var gameOverViewModel: GameOverViewModel
+    private lateinit var factory: ManageViewModels
 
     override fun onCreate() {
         super.onCreate()
-        val sharedPreferences = getSharedPreferences(
-            "quizAppData",
-            Context.MODE_PRIVATE
+        val make = ProvideViewModel.Make(
+            Core(
+                this,
+                object : ClearViewModel {
+                    override fun clear(viewModelClass: Class<out MyViewModel>) {
+                        factory.clear(viewModelClass)
+                    }
+                }
+            )
         )
-        val corrects = IntCache.Base(sharedPreferences, "corrects", 0)
-        val incorrects = IntCache.Base(sharedPreferences, "incorrects", 0)
-        gameViewModel = GameViewModel(
-            GameRepository.Base(
-                corrects,
-                incorrects,
-                IntCache.Base(sharedPreferences, "indexKey", 0),
-                IntCache.Base(sharedPreferences, "userChoiceIndexKey", -1)
-        ))
-        gameOverViewModel = GameOverViewModel(StatsRepository.Base(
-            corrects,
-            incorrects
-        ))
+
+        factory = ManageViewModels.Factory(make)
     }
+
+    override fun <T : MyViewModel> makeViewModel(clasz: Class<T>): T {
+        return factory.makeViewModel(clasz)
+    }
+
+
 }
 
 
