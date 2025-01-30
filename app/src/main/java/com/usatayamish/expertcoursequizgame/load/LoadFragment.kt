64df45c3784f06a2b9dcf64a80ee2b2aafbf47d8
usatayamish <147038989+LuftWaiffu@users.noreply.github.com>
2025-01-30 14:14:25 +0300

    QG-21 permissions and LoadFragment

---
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/load/LoadFragment.kt
@@ -0,0 +1,65 @@
+package com.usatayamish.expertcoursequizgame.load
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.usatayamish.expertcoursequizgame.ProvideViewModel
+import com.usatayamish.expertcoursequizgame.databinding.FragmentLoadBinding
+
+class LoadFragment : Fragment(){
+
+    private var _binding: FragmentLoadBinding? = null
+
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoadBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private val update: (LoadUiState) -> Unit = { uiState ->
+        uiState.show(
+            binding.errorTextView,
+            binding.retryButton,
+            binding.progressBar
+        )
+    }
+
+    private lateinit var viewModel: LoadViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel =
+            (requireActivity() as ProvideViewModel).makeViewModel(LoadViewModel::class.java)
+
+        binding.retryButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.load(isFirstRun = savedInstanceState == null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.startUpdates(observer = update)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        viewModel.stopUpdates()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/usatayamish/expertcoursequizgame/load/LoadScreen.kt
@@ -0,0 +1,5 @@
+package com.usatayamish.expertcoursequizgame.load
+
+import com.usatayamish.expertcoursequizgame.Screen
+
+object LoadScreen: Screen.Replace(LoadFragment::class.java)
